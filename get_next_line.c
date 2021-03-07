c0948c74850df2800b86316dda3034809dc4754b
Felipe Tadeu de souza lima <ftadeu-d@c1r2p1.42sp.org.br>
2021-03-07 15:08:58 -0300

    still correcting norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,34 @@
 /*   By: ftadeu <ftadeu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 18:44:13 by ftadeu            #+#    #+#             */
-/*   Updated: 2021/03/06 19:11:00 by ftadeu           ###   ########.fr       */
+/*   Updated: 2021/03/07 15:07:36 by ftadeu-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include	"get_next_line.h"
 
-static void     ft_strdel(char **str)
+static void		ft_strdel(char **str)
 {
-        if(*str && str)
-        {
-            free(*str);
-            *str = NULL;
-        }
+	if (*str && str)
+	{
+		free(*str);
+		*str = NULL;
+	}
 }
 
-static int      return_line(int rd, char ** heap, char **line)
+static int		return_line(int rd, char ** heap, char **line)
 {
-    int     i;
-    char    *tmp;
-
-    i = 0;
-    if(rd < 0)
-        return(-1);
+	int		i;
+	char	*tmp;
+	
+	i = 0;
+	if (rd < 0)
+		return (-1);
     else if (rd == 0 && *heap[0] == '\0')
     {
         *line = ft_strdup(STR_EMPTY);
         ft_strdel(heap);
-        return(0);
+        return (0);
     }
     while ((*heap)[i] != LBREAK && (*heap)[i] != '\0')
         i++;
@@ -46,10 +46,10 @@ static int      return_line(int rd, char ** heap, char **line)
         return (1);
     }
     ft_strdel(heap);
-    return(0);
+    return (0);
 }
 
-int         get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
     int         rd;
     char        *tmp;
@@ -58,12 +58,12 @@ int         get_next_line(int fd, char **line)
 
     if ((fd < 0) || !line || BUFFER_SIZE < 1 || (read(fd, NULL, 0) < 0))
         return (-1);
-    if(!(heap[fd]))
+    if (!(heap[fd]))
     {
-        if(!(heap[fd] = ft_strdup(STR_EMPTY)))
+        if (!(heap[fd] = ft_strdup(STR_EMPTY)))
             return (-1);
     }
-    if(!(buffer = malloc((BUFFER_SIZE + 1) * sizeof(*buffer))))
+    if (!(buffer = malloc((BUFFER_SIZE + 1) * sizeof(*buffer))))
         return (-1);
     while ((rd = read(fd, buffer, BUFFER_SIZE)) > 0)
     {
@@ -71,7 +71,7 @@ int         get_next_line(int fd, char **line)
         tmp = ft_strjoin(heap[fd], buffer);
         ft_strdel(&heap[fd]);
         heap[fd] = tmp;
-        if(ft_strchr(buffer, LBREAK))
+        if (ft_strchr(buffer, LBREAK))
             break;
     }
     free(buffer);
--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,25 @@
 /*   By: ftadeu <ftadeu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 18:58:47 by ftadeu            #+#    #+#             */
-/*   Updated: 2021/03/06 19:10:59 by ftadeu           ###   ########.fr       */
+/*   Updated: 2021/03/07 13:54:49 by ftadeu-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
-#define LBREAK  '\n'
-#define STR_EMPTY ""
+# define LBREAK '\n'
+# define STR_EMPTY ""
 
-int         get_next_line(int fd, char ** line);
-char	    *ft_strdup(const char *s1);
-char	    *ft_strchr(const char *s, int c);
-char	    *ft_strjoin(char const *s1, char const *s2);
-char	    *ft_substr(char const *s, unsigned int start, size_t len);
-size_t      ft_strlen(const char *s);
+int			get_next_line(int fd, char **line);
+char		*ft_strdup(const char *s1);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+size_t		ft_strlen(const char *s);
 
 #endif
